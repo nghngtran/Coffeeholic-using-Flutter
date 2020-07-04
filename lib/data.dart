dbafc784c2ff8012de2589cd5219cb8bbf5a9564
Nguyen Hoang Ngoc Tran <nghngtran68@gmail.com>
2020-07-04 13:10:51 +0700

    init ver

---
+++ lib/data.dart
@@ -0,0 +1,71 @@
+//List<String> images = [
+//  "assets/image_6.jpg",
+//  "assets/image_5.jpg",
+//  "assets/image_4.jpg",
+//  "assets/image_3.jpg",
+//  "assets/image_2.jpg",
+//  "assets/image_1.jpg",
+//];
+//
+//List<String> title = [
+//  "Sấu đá - Thức uống Hà Nội giải nhiệt mùa hè",
+////  "Mintown Roasting Coffee - Thưởng thức cà phê chuẩn mực cao cấp taị Đà Nẵng",
+//  "Thinker & Dreamer - Một tình yêu đẹp có vị giống một ly cà phê ngon",
+//  "Brew Man - Mùi hương gợi nhớ về kí ức của bạn",
+//  "LÀ VIỆT Coffee - Là Việt giữa lòng Hà Nội nhiêu yêu thương",
+////  "HAAN Drink & Eatery - Brunch sang chảnh giữa lòng Sài Gòn",
+//"Mintown Roasting Coffee - Thưởng thức cà phê chuẩn mực cao cấp taị Đà Nẵng",
+//  "Boulevard - Không chỉ đơn thuần là cà phê, mà còn là tinh túy",
+//];
+class CoffeeInfo {
+  final String position;
+  final String name;
+  final String iconImage;
+  final String description;
+  final List<String> images;
+
+  CoffeeInfo ({
+      this.position,
+        this.name,
+        this.iconImage,
+        this.description,
+        this.images,
+      });
+}
+List<CoffeeInfo> data= [
+  CoffeeInfo(
+    position: "178 Lê Hồng Phong, Hải Châu, Đà Nẵng",
+    name: "Boulevard Icecream & Coffee - Địa điểm sống ảo mới toang trông cực Hàn Xẻng",
+    iconImage: "assets/image_1.jpg",
+    description: "Có một không gian nhỏ bé, xinh xắn tại một góc phố. Ánh nắng chói chang, xiên qua những ô cửa. Chúng mình vẫn ở đây, góp nhặt những khoảnh khắc. Ngày em đẹp nhất là ngày em nở nụ cười.",
+    images: ["assets/image_13.jpg","assets/image_14.jpg","assets/image_15.jpg"]
+  ),
+  CoffeeInfo(
+      position: "73 Bến Nghé, Quận 1, TP HCM",
+      name: "Hanoi Egg Coffee - Sấu đá - Thức uống Hà Nội giải nhiệt mùa hè",
+      iconImage: "assets/image_6.jpg",
+      description: "Hà Nội tháng 7- mùa vắng những cơn mưa,mùa của cái nắng nóng oi ả và mùa của những quả sấu non. Hãy ghé tiệm của chúng tớ để thưởng thức ly sấu đá chua ngọt mát lạnh giữa lòng Sài Gòn.",
+      images: ["assets/image_9.jpg","assets/image_7.jpg","assets/image_8.jpg"]
+  ),
+  CoffeeInfo(
+      position: "16 Tăng Bạt Hổ, Hoàn Kiếm, Hà Nội",
+      name: "LÀ VIỆT Coffee giữa lòng Hà Nội nhiêu yêu thương",
+      iconImage: "assets/image_19.jpg",
+      description: "Specialty coffee hay còn gọi là làn sóng cà phê thứ 3, xuất hiện đầu tiên trên thế giới vào năm 1970. Không chỉ là tên gọi của một loại cà phê mà còn cả một quy trình chế biến đặc biệt. Tuyển chọn khắt khe từng hạt cà phê theo tiêu chuẩn cho đến công thức, kỹ thuật rang, xay và pha chế công phu, tỉ mỉ.",
+      images: ["assets/image_19.jpg","assets/image_20.jpg","assets/image_21.jpg"]
+  ),
+  CoffeeInfo(
+      position: "166 đường 2/9, Hải Châu, Đà Nẵng",
+      name: "Mintown Coffee Roasting - Không chỉ là cà phê thuần túy ",
+      iconImage: "assets/image_4.jpg",
+      description: "Specialty coffee hay còn gọi là làn sóng cà phê thứ 3, xuất hiện đầu tiên trên thế giới vào năm 1970. Không chỉ là tên gọi của một loại cà phê mà còn cả một quy trình chế biến đặc biệt. Tuyển chọn khắt khe từng hạt cà phê theo tiêu chuẩn cho đến công thức, kỹ thuật rang, xay và pha chế công phu, tỉ mỉ.",
+      images: ["assets/image_16.jpg","assets/image_17.jpg","assets/image_18.jpg"]
+  ),
+  CoffeeInfo(
+      position: "21B Lê Lợi, quận 1, TP HCM",
+      name: "Thinker & Dreamer - Kẻ ôm mộng mơ giữa lòng Sài Thành",
+      iconImage: "assets/image_3.jpg",
+      description: "Một tình yêu đẹp có vị giống một ly cà phê ngon. Có đủ đắng, chua, chát. Và hậu vị ngọt béo thật dài. Tùy vào mỗi người mà hương vị được gia giảm khác nhau. Cuối cùng, thành quả là ly cà phê đầy bí ẩn, mời gọi ta nếm thử. Có ly ngon nhưng quá nặng đô, không thể đi cùng hết đoạn đường dài. Có ly vừa đủ, đủ bình yên và đủ gắn bó cả đời.",
+      images: ["assets/image_11.jpg","assets/image_10.jpg","assets/image_12.jpg"]
+  )
+];
---
+++ lib/detail_page.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+import 'data.dart';
+import 'data.dart';
+
+
+Color primaryTextColor = Color(0xFF414C6B);
+Color secondaryTextColor = Color(0xFFE4979E);
+Color titleTextColor = Colors.white;
+Color contentTextColor = Color(0xff868686);
+
+class DetailPage extends StatelessWidget {
+  final CoffeeInfo coffeeInfo;
+
+  const DetailPage(CoffeeInfo _coffeeInfo) : coffeeInfo = _coffeeInfo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: <Widget>[
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+        Container(margin: EdgeInsets.only(top:50),
+                        height: 250,width: MediaQuery.of(context).size.width,child:Image.asset(coffeeInfo.iconImage,fit: BoxFit.fitWidth),
+                      ),
+                  Padding(
+                    padding:EdgeInsets.all(32.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+//                        SizedBox(height: 300),
+//                      Container(margin: EdgeInsets.only(left: 0),
+//                        height: 250,width: MediaQuery.of(context).size.width,child:Image.asset(coffeeInfo.iconImage,fit: BoxFit.fitWidth),
+//                      ),
+                        Text(
+                          coffeeInfo.name,
+                          style: TextStyle(
+                            fontFamily: 'Avenir',
+                            fontSize: 32,
+                            color: primaryTextColor,
+                            fontWeight: FontWeight.w900,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                        Text(
+                          coffeeInfo.position,
+                          style: TextStyle(
+                            fontFamily: 'Avenir',
+                            fontSize: 24,
+                            color: primaryTextColor,
+                            fontWeight: FontWeight.w300,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                        Divider(color: Colors.black38),
+                        SizedBox(height: 32),
+                        Text(
+                          coffeeInfo.description ?? '',
+                          maxLines: 5,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontFamily: 'Avenir',
+                            fontSize: 20,
+                            color: contentTextColor,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: 32),
+                        Divider(color: Colors.black38),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 32.0),
+                    child: Text(
+                      'Gallery',
+                      style: TextStyle(
+                        fontFamily: 'Avenir',
+                        fontSize: 25,
+                        color: const Color(0xff47455f),
+                        fontWeight: FontWeight.w300,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  Container(
+                    height: 250,
+                    padding: const EdgeInsets.only(left: 32.0),
+                    child: ListView.builder(
+                        itemCount: coffeeInfo.images.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            clipBehavior: Clip.antiAlias,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                            child: AspectRatio(
+                                aspectRatio: 1,
+                                child: Image.asset(
+                                  coffeeInfo.images[index],
+                                  fit: BoxFit.cover,
+                                )),
+                          );
+                        }),
+                  ),
+                ],
+              ),
+            ),
+//            Positioned(
+//              child: Hero(
+//                  tag: coffeeInfo.position,
+//                  child:  Container(margin: EdgeInsets.only(top: 50),
+//                        height: 280,width: MediaQuery.of(context).size.width,child:Image.asset(coffeeInfo.iconImage,fit: BoxFit.fitWidth),
+//                      ),
+//            )),
+//            Positioned(
+//              top: 60,
+//              left: 32,
+//              child: Text(
+//                coffeeInfo.position.toString(),
+//                style: TextStyle(
+//                  fontFamily: 'Avenir',
+//                  fontSize: 247,
+//                  color: primaryTextColor.withOpacity(0.08),
+//                  fontWeight: FontWeight.w900,
+//                ),
+//                textAlign: TextAlign.left,
+//              ),
+//            ),
+            IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
